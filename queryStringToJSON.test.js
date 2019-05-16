ed2801eb6cf0ebc63cf33ef16855e8a8ef68dfba
Neha Saggam <nehasaggam@gmail.com>
2019-05-17 00:07:19 +0530

    Resolve code styling issues

--- queryStringToJSON.test.js
+++ queryStringToJSON.test.js
@@ -1,17 +1,17 @@
-import queryStringToJSON from './queryStringToJSON';
+import queryStringToJSON from "./queryStringToJSON";
 
 test("Given 'a=1&b=2' returns { 'a': '1', 'b': '2' }", () => {
-    const input = 'a=1&b=2';
+    const input = "a=1&b=2";
     const output = queryStringToJSON(input);
     const expectedOutput = {
-        'a': '1',
-        'b': '2'
+        "a": "1",
+        "b": "2"
     };
     expect(output).toStrictEqual(expectedOutput);
 });
 
 test("Given 'a=1&b=2' should not return { 'a': 1, 'b': 2 }", () => {
-    const input = 'a=1&b=2';
+    const input = "a=1&b=2";
     const output = queryStringToJSON(input);
     const expectedOutput = {
         a: 1,
@@ -21,66 +21,66 @@ test("Given 'a=1&b=2' should not return { 'a': 1, 'b': 2 }", () => {
 });
 
 test("Given 'a[]=1&a[]=2' returns { a: ['1', '2'] }", () => {
-    const input = 'a[]=1&a[]=2';
+    const input = "a[]=1&a[]=2";
     const output = queryStringToJSON(input);
-    const expectedOutput = { a : ['1', '2'] };
+    const expectedOutput = { a : ["1", "2"] };
     expect(output).toStrictEqual(expectedOutput);
 });
 
 test("Given '&&&&crsource=google' should return { crsource: 'google' }", () => {
-    const input = '&&&&crsource=google';
+    const input = "&&&&crsource=google";
     const output = queryStringToJSON(input);
     const expectedOutput = {
-        crsource: 'google'
+        crsource: "google"
     };
     expect(output).toStrictEqual(expectedOutput);
 });
 
 test("Given '&&&&crsource=google to              ' should return { crsource: 'google to              ' }", () => {
-    const input = '&&&&crsource=google to              ';
+    const input = "&&&&crsource=google to              ";
     const output = queryStringToJSON(input);
     const expectedOutput = {
-        crsource: 'google to              '
+        crsource: "google to              "
     };
     expect(output).toStrictEqual(expectedOutput);
 });
 
 test("Given '&&     &&crsource=google' should return { crsource: 'google' }", () => {
-    const input = '&&     &&crsource=google';
+    const input = "&&     &&crsource=google";
     const output = queryStringToJSON(input);
     const expectedOutput = {
-        crsource: 'google'
+        crsource: "google"
     };
     expect(output).toStrictEqual(expectedOutput);
 });
 
 test("Given '&&&&    crsource=google' should return { crsource: 'google' }", () => {
-    const input = '&&&&    crsource=google';
+    const input = "&&&&    crsource=google";
     const output = queryStringToJSON(input);
     const expectedOutput = {
-        crsource: 'google'
+        crsource: "google"
     };
     expect(output).toStrictEqual(expectedOutput);
 });
 
 test("Given 'a[]=1&a[]=2&b=[]=3' returns { a: ['1', '2'], b: ['3'] }", () => {
-    const input = 'a[]=1&a[]=2&b[]=3';
+    const input = "a[]=1&a[]=2&b[]=3";
     const output = queryStringToJSON(input);
-    const expectedOutput = { a : ['1', '2'], b: ['3'] };
+    const expectedOutput = { a : ["1", "2"], b: ["3"] };
     expect(output).toStrictEqual(expectedOutput);
 });
 
 
 test("Given 'a[]=1&a[]=2&b=[]=3&c=4' returns { a: ['1', '2'], b: ['3'], c: '4' }", () => {
-    const input = 'a[]=1&a[]=2&b[]=3&c=4';
+    const input = "a[]=1&a[]=2&b[]=3&c=4";
     const output = queryStringToJSON(input);
-    const expectedOutput = { a : ['1', '2'], b: ['3'], c: '4' };
+    const expectedOutput = { a : ["1" , "2"], b: ["3"], c: "4" };
     expect(output).toStrictEqual(expectedOutput);
 });
 
 test("Given 'a[]=1&b[]=2&a=[]=3' returns { a: ['1', '3'], b: ['2'] }", () => {
-    const input = 'a[]=1&b[]=2&a[]=3';
+    const input = "a[]=1&b[]=2&a[]=3";
     const output = queryStringToJSON(input);
-    const expectedOutput = { a : ['1', '3'], b: ['2'] };
+    const expectedOutput = { a : ["1", "3"], b: ["2"] };
     expect(output).toStrictEqual(expectedOutput);
 });
